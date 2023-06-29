387e072cc507e9ecac072ca01931798c1459de04
Niffxd <nicosanchez673@gmail.com>
2023-06-28 21:01:20 -0300

    updating css

--- src/pages/JobDetail/JobDetail.jsx
+++ src/pages/JobDetail/JobDetail.jsx
@@ -1,12 +1,26 @@
 import { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom'; //eslint-disable-line
 import style from './JobDetail.module.css';
+import './buttons.css';
 
 export default function JobPageDetail () {
   const { state } = useLocation();
   const navigate = useNavigate();
 
+  const [actualImg, setImg] = useState(null);
   const [job, setJob] = useState(null);
+
+  const handlerPreview = (image) => {
+    setImg(null);
+    setImg(image);
+    document.getElementById('preview_image')?.showModal();
+    document.getElementById('preview_image')?.classList.add(style.show_modal);
+  };
+
+  const handleCloseModal = () => {
+    document.getElementById('preview_image').close();
+    document.getElementById('preview_image').classList.remove(style.show_modal);
+  };
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -36,12 +50,52 @@ export default function JobPageDetail () {
         {
           job?.images.map(image => {
             return (
-              <img key={image} src={image} alt={image} />
+              <img onClick={() => handlerPreview(image)} key={image} src={image} alt={image} />
             );
           })
         }
       </div>
       <button className={style.button_back} onClick={() => navigate(-1)}>◀ Atrás</button>
+      {
+        job
+          ? <dialog id='preview_image' className={style.preview__container}>
+              <div className={style.preview__content}>
+                <div className={style.preview_image}>
+                  <img src={actualImg} alt={actualImg} />
+                  <div className={style.controls}>
+                    {
+                      job?.images?.indexOf(actualImg) > 0
+                        ? <button onClick={() => setImg(job.images[(job?.images?.indexOf(actualImg)) - 1])}>
+                            <span className='material-symbols-outlined'>
+                              chevron_left
+                            </span>
+                          </button>
+                        : <button onClick={() => setImg(job.images[(job?.images?.length) - 1])}>
+                            <span className='material-symbols-outlined'>
+                              chevron_left
+                            </span>
+                          </button>
+                    }
+                    {
+                      job?.images?.indexOf(actualImg) < job?.images?.length - 1
+                        ? <button onClick={() => setImg(job.images[(job?.images?.indexOf(actualImg)) + 1])}>
+                            <span className='material-symbols-outlined'>
+                              chevron_right
+                            </span>
+                          </button>
+                        : <button onClick={() => setImg(job.images[0])}>
+                            <span className='material-symbols-outlined'>
+                              chevron_right
+                            </span>
+                          </button>
+                      }
+                  </div>
+                </div>
+                <button onClick={handleCloseModal} id='cancel'>Volver</button>
+              </div>
+            </dialog>
+          : ''
+      }
     </div>
   );
 }
