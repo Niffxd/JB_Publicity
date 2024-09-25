7ed5cdf33c0cdfa221ece24a3b6ed6a1b8c2f783
Niffxd <nicosanchez673@gmail.com>
2024-09-25 01:52:34 -0300

    update: changing logos, favicon and 404 image page

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom'; //eslint-disable-line
+import notFound from '../../assets/images/logo/404.svg';
 import style from './NotFoundPage.module.css';
 
-export default function NotFoundPage ({ logo, categories }) {
+export default function NotFoundPage ({ categories }) {
   return (
     <div className={style.not_found__container}>
-      <img src={logo} alt='logo' />
+      <img src={notFound} alt='logo'/>
       <div className={style.category__container}>
         <h2>¿Te perdiste?</h2>
         <ul className={style.category_list}>
